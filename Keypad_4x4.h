ed187ed2971e2950632f33b68902fac5b897c301
Goldan32 <50270724+Goldan32@users.noreply.github.com>
2019-06-20 21:07:35 +0200

    Update Keypad_4x4.h

--- Keypad_4x4.h
+++ Keypad_4x4.h
@@ -1,17 +1,17 @@
 /*
-Library:					4x4 keypad drive for STM32 MCUs
-Written by:					Daniel Arany
+Library:				4x4 keypad drive for STM32 MCUs
+Written by:				Daniel Arany
 Date written:				20/06/2019
 Description:				The pins of the keypad must be set up in the user's code.
-							Pin numbers (from right to left) are: OUT 0-3,IN 0-3.
-							The input pins must be set to Pull-Down in configuration
-							(without such option use external resistors ~1k-10k).
-Functions:					Keypad_Init:			Initializes the library. A user defined variable
-													must be passed with Keypad_WiresTypeDef type.
-							Keypad_ReadKey:			Returns number of buttons pressed and sets 
-													the bool array correctly.
-							Keypad_GetNumber:		Returns the number (HEX) pressed as int.
-							Keypad_GetCharacter:	Returns the character pressed as char.
+					Pin numbers (from right to left) are: OUT 0-3,IN 0-3.
+					The input pins must be set to Pull-Down in configuration
+					(without such option use external resistors ~1k-10k).
+Functions:				Keypad_Init:		Initializes the library. A user defined variable
+								must be passed with Keypad_WiresTypeDef type.
+					Keypad_ReadKey:		Returns number of buttons pressed and sets 
+								the bool array correctly.
+					Keypad_GetNumber:	Returns the number (HEX) pressed as int.
+					Keypad_GetCharacter:	Returns the character pressed as char.
 */
 
 //Header includes
